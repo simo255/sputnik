663e15916fb5ed8b7960366273b1181186e371df
simo255 <bmohamed255@gmail.com>
2024-02-10 18:39:41 -0500

    test webhooks

--- src/main/java/pl/touk/sputnik/squareNumber.java
+++ src/main/java/pl/touk/sputnik/squareNumber.java
@@ -1,7 +1,6 @@
 package pl.touk.sputnik;
 
 public class squareNumber {
-    // test github webhooks
     public int square(int number) {
         return number * number;
     }
--- src/test/java/pl/touk/sputnik/squareNumberTest.java
+++ src/test/java/pl/touk/sputnik/squareNumberTest.java
@@ -7,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SquareNumberTest {
     @Test
     void shouldReturnSquareOfPositiveNumber() {
-        // given
         int number = 5;
         squareNumber squareNumber = new squareNumber();
 
